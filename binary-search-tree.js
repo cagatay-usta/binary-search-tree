f6dd3625b239b1940b208efc81788842179b8404
cagatay-usta <cagatay_usta@windowslive.com>
2023-06-07 21:33:02 +0300

    add delete, find and levelOrder methods

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,7 +1,7 @@
 // binary search tree
 
 class Node {
-  constructor(data) {
+  constructor(data = null) {
     this.data = data;
     this.left = null;
     this.right = null;
@@ -10,7 +10,8 @@ class Node {
 
 export default class Tree {
   constructor(array) {
-    this.root = this.buildTree(this.sanitize(array));
+    if (!array) this.root = new Node();
+    else this.root = this.buildTree(this.sanitize(array));
   }
 
   // removes duplicates and sorts the array
@@ -30,7 +31,62 @@ export default class Tree {
   }
 
   insert(data, node = this.root) {
+    if (!node.data) return (node.data = data);
     if (data < node.data) return node.left ? this.insert(data, node.left) : (node.left = new Node(data));
     if (data > node.data) return node.right ? this.insert(data, node.right) : (node.right = new Node(data));
+  }
+  delete(data, root = this.root) {
+    this.root = this.deleteNode(data, root);
+  }
+  deleteNode(data, node) {
+    if (data === node.data) {
+      // base case = no children
+      if (!node.left && !node.right) return null;
+      // only right child
+      if (!node.left && node.right) return node.right;
+      // only left child
+      if (node.left && !node.right) return node.left;
+      // has two children
+      else {
+        let prev = node;
+        let cur = node.right;
+        // change variable to indicate which pointer will be updated later
+        let change = false;
+        while (cur.left) {
+          change = true;
+          prev = cur;
+          cur = cur.left;
+        }
+        node.data = cur.data;
+        // if it's the first child deleted update the right child, if it has more depth update the pointer to the left
+        change ? (prev.left = cur.left) : (prev.right = cur.right);
+        return node;
+      }
+    }
+    if (data < node.data) node.left = this.deleteNode(data, node.left);
+    if (data > node.data) node.right = this.deleteNode(data, node.right);
+    return node;
+  }
+
+  find(data, node = this.root) {
+    if (data < node.data) return this.find(data, node.left);
+    if (data > node.data) return this.find(data, node.right);
+    if (data === node.data) return node;
+    // if not found return null
+    return null;
+  }
+
+  levelOrder(node = this.root) {
+    if (!node) return;
+    const queue = [];
+    const values = [];
+    queue.push(node);
+    while (queue.length) {
+      const current = queue.shift();
+      values.push(current.data);
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+    }
+    return values;
   }
 }
--- main.js
+++ main.js
@@ -5,7 +5,26 @@ import prettyPrint from "./prettyprint.js";
 const testArray = [3, 5, 6, 1, 2, 4, 7, 8, 9];
 const testArray2 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = new Tree(testArray2);
-console.log(tree);
-prettyPrint(tree.root);
 tree.insert(2);
+console.log(tree);
+console.log(tree.levelOrder());
 prettyPrint(tree.root);
+
+const tree2 = new Tree();
+tree2.insert(3);
+tree2.insert(2);
+tree2.insert(4);
+tree2.insert(5);
+tree2.insert(8);
+tree2.insert(9);
+tree2.insert(7);
+// tree2.delete(8);
+// tree2.delete(2);
+// tree2.delete(4);
+
+// tree2.insert(4);
+// tree2.delete(3);
+console.log(tree2);
+console.log(tree2.levelOrder());
+console.log(tree2.find(8));
+prettyPrint(tree2.root);
